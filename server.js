41c35da58ef4572889aae3ebe180dc80831e2b81
marekhakala <hakala.marek@gmail.com>
2017-10-13 18:13:27 +0200

    Init commit

---
+++ server.js
@@ -0,0 +1,72 @@
+/*
+  Copyright 2017 <hakala.marek@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import fs from "fs";
+import http from "http";
+import path from "path";
+import methods from "methods";
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
+import errorhandler from "errorhandler";
+
+const nodeEnv = process.env.NODE_ENV || "development";
+const isProduction = nodeEnv === "production";
+const isDevelopment = nodeEnv === "development";
+const appConfig = require("./config/env.json")[nodeEnv];
+
+const app = express();
+app.use(cors());
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("method-override")());
+app.use(session({ secret: "addressbookapi", cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+if(!isProduction) { app.use(errorhandler()); }
+
+app.use(require("./app/routes"));
+
+/// Error 404 handler
+app.use((req, res, next) => {
+  res.status(404).end("Not Found");
+  next();
+});
+
+// Development Error Handler
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+    res.json({ "errors": { error: err, message: err.message }});
+  });
+}
+
+// Production Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+});
+
+// Start server
+const server = app.listen(process.env.PORT || appConfig["PORT"], () => {
+  console.log("Starting server ... ;)");
+  console.log("NODE_ENV=" + nodeEnv);
+  console.log("Listening on port " + server.address().port);
+});
+
+module.exports = server;
